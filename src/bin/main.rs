5b4397fa2bc619b09dc74841aebb841687871b77
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-04-12 15:50:23 +0200

    Add parsers : +, *

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 use message_friendly_maths_writer::*;
 
 fn main() {
-    println!("{:?}", parse_expression("x^23"));
+    println!("{:?}", parse_expression("x^2+x^3"));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ use nom::{
 pub enum Expression<'a>{
     Num(&'a str),
     Var(&'a str),
+    Plus((Box<Expression<'a>>, Box<Expression<'a>>)),
+    Multiply((Box<Expression<'a>>, Box<Expression<'a>>)),
     Power((Box<Expression<'a>>, Box<Expression<'a>>)),
 }
 
@@ -23,15 +25,35 @@ fn parse_var(i: &str) -> IResult<&str, Expression> {
     Ok((i, Expression::Var(expr)))
 }
 
+fn parse_atom(i: &str) -> IResult<&str, Expression> {
+    alt((
+            parse_num,
+            parse_var,
+    ))(i)
+}
+
+
 fn parse_power(i: &str) -> IResult<&str, Expression> {
-    let (i, (base, exp)) = separated_pair(parse_var, tag("^"), parse_num)(i)?;
+    let (i, (base, exp)) = separated_pair(parse_atom, tag("^"), parse_expression)(i)?;
     Ok((i, Expression::Power((Box::new(base), Box::new(exp)))))
+}
+
+fn parse_plus(i: &str) -> IResult<&str, Expression> {
+    let (i, (base, exp)) = separated_pair(parse_atom, tag("+"), parse_expression)(i)?;
+    Ok((i, Expression::Plus((Box::new(base), Box::new(exp)))))
+}
+
+fn parse_multiply(i: &str) -> IResult<&str, Expression> {
+    let (i, (base, exp)) = separated_pair(parse_atom, tag("*"), parse_expression)(i)?;
+    Ok((i, Expression::Plus((Box::new(base), Box::new(exp)))))
 }
 
 
 pub fn parse_expression(i: &str) -> IResult<&str, Expression> {
     alt((
             parse_power,
+            parse_multiply,
+            parse_plus,
             parse_num,
             parse_var,
     ))(i)
